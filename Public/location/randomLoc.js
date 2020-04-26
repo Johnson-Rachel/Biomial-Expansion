0a67117299eaccc8bf4a7005d66b611d1544010d
Rachel Johnson <56367810+Johnson-Rachel@users.noreply.github.com>
2020-04-25 22:31:01 -0500

    Add files via upload
    Adding a page for each biome and a script to open one of these pages randomly

---
+++ Public/location/randomLoc.js
@@ -0,0 +1,30 @@
+<script type="text/javascript'>
+function randomLoc(){
+    var myrandom=Math.round(Math.random()*7)
+    
+    var link1="001.html"
+    var link2="002.html"
+    var link3="003.html"
+    var link4="004.html"
+    var link5="005.html"
+    var link6="006.html"
+    var link7="007.html"
+    var link8="008.html"
+    if (myrandom==0)
+        window.location=link1
+    else if (myrandom==1)
+        window.location=link2
+    else if (myrandom==2)
+        window.location=link3
+    else if (myrandom==3)
+        window.location=link4
+    else if (myrandom==4)
+        window.location=link5
+    else if (myrandom==5)
+        window.location=link6
+    else if (myrandom==6)
+        window.location=link7  
+    else if (myrandom==7)
+        window.location=link8       
+}
+</script>
